ea9c53402efde12e0e75defbcf1ff9773fa838df
Michael Douglas <44294286+MichaelDouglasPIX@users.noreply.github.com>
2024-09-20 19:54:56 -0300

    feat: filter by platform

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -6,7 +6,7 @@ import { gamingPlatforms } from "../models/index.js";
 class GameController {
     static async listGames(req, res, next) {
         try {
-            const searchGames = game.find().populate("genre engine stores.gamingPlatforms");
+            const searchGames = game.find();
 
             req.result = searchGames;
 
@@ -28,7 +28,7 @@ class GameController {
     static async listGameById(req, res, next) {
         try {
             const id = req.params.id;
-            const gameFound = await game.findById(id).populate("genre engine stores.gamingPlatforms").exec();
+            const gameFound = await game.findById(id).exec();
 
             if (gameFound !== null) {
                 res.status(200).json(gameFound);
@@ -75,7 +75,7 @@ class GameController {
             const search = await formatSearch(req.query);
 
             if (search !== null) {
-                const gamesByFilter = game.find(search).populate("genre engine stores.gamingPlatforms");
+                const gamesByFilter = game.find(search);
                 req.result = gamesByFilter;
                 next();
             } else {
@@ -94,6 +94,16 @@ async function formatSearch(params) {
 
     if (status) search.status = { $regex: status, $options: "i" };
     if (title) search.title = { $regex: title, $options: "i" };
+
+    if (platform) {
+        const platformFound = await gamingPlatforms.findOne({ platform });
+
+        if (platformFound !== null) {
+            search['stores.gamingPlatforms'] = platformFound._id;
+        } else {
+            search = null;
+        }
+    }
 
     if (engineName) {
         const engineFound = await engine.findOne({ name: engineName });
--- src/models/Game.js
+++ src/models/Game.js
@@ -1,18 +1,20 @@
 import mongoose from "mongoose";
+import autopopulate from "mongoose-autopopulate";
 
 const gameSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
     title: { type: String, required: [true, "title is mandatory"] },
     summary: { type: String, required: [true, "summary is mandatory"] },
     description: { type: String },
-    genre: [{ type: mongoose.Schema.Types.ObjectId, ref: 'genre', required: [true, "genre is mandatory"] }],
+    genre: [{ type: mongoose.Schema.Types.ObjectId, ref: 'genre', required: [true, "genre is mandatory"], autopopulate: { select: "name" } }],
     status: { type: String, required: [true, "status is mandatory"], enum: {values: ["released", "beta", "alpha", "demo", "developing"], message: "status {VALUE} is not allowed, allowed statuses: released, beta, alpha, demo and developing"} },
     releaseDate: { type: Date, min: ['2000-01-01', "the date {VALUE} is lower than allowed 2000-01-01"], max: ['2050-01-01', "the date {VALUE} is greater than allowed 2050-01-01"] },
-    engine: { type: mongoose.Schema.Types.ObjectId, ref: 'engine', required: [true, "engine is mandatory"] },
-    stores: [{ url: { type: String, required: [true, "store url is mandatory"] }, gamingPlatforms: { type: mongoose.Schema.Types.ObjectId, ref: 'gaming_Platforms', required: [true, "store gaming platforms is mandatory"] }, _id: false }],
+    engine: { type: mongoose.Schema.Types.ObjectId, ref: 'engine', required: [true, "engine is mandatory"], autopopulate: true },
+    stores: [{ url: { type: String, required: [true, "store url is mandatory"] }, gamingPlatforms: { type: mongoose.Schema.Types.ObjectId, ref: 'gaming_Platforms', required: [true, "store gaming platforms is mandatory"], autopopulate: true }, _id: false }],
     images: [{ name: { type: String, required: [true, "image name is mandatory"] }, url: { type: String, required: [true, "image url is mandatory"] }, _id: false }],
 }, { versionKey: false });
 
+gameSchema.plugin(autopopulate);
 const game = mongoose.model("game", gameSchema);
 
 export default game;
